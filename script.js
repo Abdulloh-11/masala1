ac67c871c70733b78a8cda057e04725d4cdee5fe
Abdulloh-11 <abdullohgulomov1234@gmail.com>
2022-12-04 23:58:27 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var name = prompt("YOU NAME");
+var summ = Number(prompt(`${name} HOW MUCH MONEY YOU HAVE`));
+if (summ >= 11100.61*250 +11100.61*500+12247.79*120) {
+  alert(` ${name} Bon Voyage`);
+} else {
+  alert(`${name} you need to be a little patient.`);
+}
